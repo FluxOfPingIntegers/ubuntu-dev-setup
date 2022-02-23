d02fefcb36e68fb0d5187e477586af00b0aff60f
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-02-23 10:45:09 -0500

    adding sqlite3, nodejs, and npm to setup-script

--- setup-script.sh
+++ setup-script.sh
@@ -1,2 +1,10 @@
-sudo apt-get update
+sudo apt update
 sudo apt upgrade
+
+curl -sSL https://get.rvm.io | bash -s stable --rails
+
+sudo apt install sqlite3
+
+sudo apt install nodejs
+sudo apt install npm
+
