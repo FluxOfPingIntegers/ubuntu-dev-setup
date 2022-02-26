3efb4454fbabe81eb5012c6a7fac906a77c1fcb5
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2022-02-26 07:00:50 -0500

    updating rvm commands

--- setup-script.sh
+++ setup-script.sh
@@ -1,7 +1,11 @@
 sudo apt update
 sudo apt upgrade
+sudo apt install curl
 
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 curl -sSL https://get.rvm.io | bash -s stable --rails
+source ~/.rvm/scripts/rvm
 
 sudo apt install sqlite3
 
